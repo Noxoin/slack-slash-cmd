34141ff30ac2b1ba948135a118b27b4bb6f1b893
Dixon Cheung <dixon.cheung@indexexchange.com>
2017-02-15 11:15:00 -0500

    Allow for LowerCase for referencing issues

--- app.go
+++ app.go
@@ -5,16 +5,18 @@ import (
     "fmt"
     "net/http"
     "regexp"
+    "strings"
 )
 
+var MAX_ATTACHMENTS = 20
+
 var slackToken = "FlMln4NEv5xTGc0czw9NKvQ9"
-var regex = regexp.MustCompile("[A-Z]+-\\d+")
+var regex = regexp.MustCompile("[A-Za-z]+-\\d+")
 var jiraPath = "http://jira.indexexchange.com/browse/"
 
 type SlackResponse struct {
     ResponseType string `json:"response_type"`
     Text         string `json:"text"`
-    //Attachment   []string `json:"attachment"`
     Attachments  []map[string]interface{} `json:"attachments"`
 }
 
@@ -29,24 +31,27 @@ func jira(w http.ResponseWriter, r *http.Request) {
         ResponseType: "ephemeral",
         Text: "Sorry, that didn't work. Please try again",
     }
-    errorJSON, _ := json.Marshal(errorResponse)
     if token != slackToken {
+        errorJSON, _ := json.Marshal(errorResponse)
         w.WriteHeader(http.StatusUnauthorized)
         w.Write(errorJSON)
         return
     }
     if command != "/jira" {
+        errorJSON, _ := json.Marshal(errorResponse)
         w.WriteHeader(http.StatusBadRequest)
         w.Write(errorJSON)
         return
     }
 
+    // Processing request
     response := &SlackResponse {
         ResponseType: "in_channel",
     }
     tickets := regex.FindAllString(message, -1)
-    for i := 0; i < len(tickets); i++ {
-        link := fmt.Sprintf("%s: %s%s", tickets[i], jiraPath, tickets[i])
+    for i := 0; i < len(tickets) && i < MAX_ATTACHMENTS; i++ {
+        ticket := strings.ToUpper(tickets[i])
+        link := fmt.Sprintf("%s: %s%s", ticket, jiraPath, ticket)
         attachment := make(map[string]interface{})
         attachment["text"] =  link
         response.Attachments = append(response.Attachments, attachment)
